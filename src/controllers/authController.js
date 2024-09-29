3eed3d81d7491b1ae35bce360c8e288c1b1c58b5
Alireza Dehghan <ar.dehghan.a@gmail.com>
2024-09-29 23:57:55 +0330

    feat: Implement user authentication
    Adds middleware for protecting routes, verifying JWT tokens, and checking for password changes. This ensures that only authenticated users with valid credentials can access protected routes.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,4 @@
+const {promisify} = require('util')
 const jwt = require('jsonwebtoken')
 const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
@@ -56,4 +57,25 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   })
+})
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token = ''
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1]
+  }
+  if (!token) return next(new AppError('Access denied. Please log in to continue.', 401))
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+  const user = await User.findByPk(decoded.id)
+  if (!user) return next(new AppError('User not found. Please log in again.', 401))
+
+  const changedTimestamp = parseInt(user.passwordChangedAt?.getTime() / 1000 || 0, 10)
+  if (changedTimestamp > decoded.iat)
+    return next(new AppError('Password was recently changed. Please log in again.', 401))
+
+  req.user = user.toJSON()
+
+  next()
 })
