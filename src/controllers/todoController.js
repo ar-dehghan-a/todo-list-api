7d92f5b89460b0dcede06f4469da1749d40ba09a
Alireza Dehghan <ar.dehghan.a@gmail.com>
2024-09-30 12:14:16 +0330

    feat: Add delete todo functionality
    Adds a new endpoint to delete existing todos. Only the owner of a todo can delete it. The endpoint also includes appropriate authorization checks.

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -37,4 +37,21 @@ const updateTodo = catchAsync(async (req, res, next) => {
   })
 })
 
-module.exports = {createTodo, updateTodo}
+const deleteTodo = catchAsync(async (req, res, next) => {
+  const {id} = req.params
+
+  const todo = await Todo.findByPk(id)
+
+  if (!todo) return next(new AppError('Todo not found', 404))
+
+  if (req.user.id !== todo.userId)
+    return next(new AppError('You do not have permission to update this todo.', 403))
+
+  await todo.destroy()
+
+  res.status(204).json({
+    status: 'success',
+  })
+})
+
+module.exports = {createTodo, updateTodo, deleteTodo}
--- src/routes/v1/todos.js
+++ src/routes/v1/todos.js
@@ -7,6 +7,6 @@ const router = Router()
 router.use(authController.protect)
 
 router.route('/').post(todoController.createTodo)
-router.route('/:id').patch(todoController.updateTodo)
+router.route('/:id').patch(todoController.updateTodo).delete(todoController.deleteTodo)
 
 module.exports = router
