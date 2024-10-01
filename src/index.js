b4b03b190bae641e941fceaf3e20c8f3b7ca2863
Alireza Dehghan <ar.dehghan.a@gmail.com>
2024-10-01 15:41:21 +0330

    feat: Enhance security and performance
    Introduce security measures like HTTP header hardening with Helmet and rate limiting to prevent brute force attacks. Improve performance by enabling response compression and static file serving.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const compression = require('compression')
 
 const mainRouter = require('./routes')
 
@@ -9,14 +13,26 @@ const errorController = require('./controllers/errorController')
 
 const app = express()
 
-app.use(express.json())
+// Set security HTTP headers
+app.use(helmet())
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again after 15 minutes.',
+})
+app.use(limiter)
+
+app.use(express.json({limit: '10kb'}))
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 if (process.env['NODE_ENV'] === 'development') app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-  res.send('Todo List API')
-})
+// Serving static files
+app.use(express.static(path.join(__dirname, '../', 'public')))
+
+app.use(compression())
 
 app.use('/api', mainRouter)
 
