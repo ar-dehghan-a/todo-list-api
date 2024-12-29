041ff7eb24bd50ffbc9b9f999351dfdea6c74a9c
Alireza Dehghan <ar.dehghan.a@gmail.com>
2024-12-30 01:14:38 +0330

    feat: Update pagination defaults and enhance dependency versions

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,11 +1,11 @@
 const pagination = query => {
-  let {page = 0, limit = 25} = query
+  let {page = 1, limit = 20} = query
 
   page = parseInt(page)
   limit = parseInt(limit)
 
-  page = Math.max(0, page)
-  limit = Math.min(25, Math.max(1, limit))
+  page = Math.max(1, page)
+  limit = Math.min(50, Math.max(10, limit))
 
   const offset = (page - 1) * limit
 
